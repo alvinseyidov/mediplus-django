655d4c607603ff13964a8ae7dcf89118404bef3a
Alvin Seyidzade <alvinseyidzade@gmail.com>
2019-01-26 15:10:35 -0800

    Doctors Api Edit

--- doctors/views.py
+++ doctors/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from .models import Doctors
 from rest_framework import generics
 from .serializers import DoctorsSerializer
-
-def doctors_list(request):
-    doctors = Doctors.objects.all()
-    context = {
-        'doctors':doctors
-    }
-    return render(request,'doctors.html', context)
+from django.http import JsonResponse
 
 class ListDoctorsView(generics.ListAPIView):
     queryset = Doctors.objects.all()
     serializer_class = DoctorsSerializer
+
+def doctors_list(request):
+    MAX_OBJECTS = 20
+    doctors = Doctors.objects.all()[:MAX_OBJECTS]
+    data = {"results": list(doctors.values("first_name", "last_name", "profile_image"))}
+    return JsonResponse(data)
